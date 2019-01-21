2f739b9bfd1b3e25f8380a772b9264a49ec89228
poet <ispoetmail@gmail.com>
2019-01-21 23:56:13 +0800

    add leetcode 46, method 2

--- src/main/java/com/home/leetcode/Permutations_46.java
+++ src/main/java/com/home/leetcode/Permutations_46.java
@@ -1,6 +1,5 @@
 package com.home.leetcode;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +18,7 @@ public class Permutations_46 {
      */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums ==null || nums.length == 0) return res;
+        if(nums == null || nums.length == 0) return res;
         helper(res, new ArrayList<>(), nums);
         return res;
     }
@@ -30,11 +29,47 @@ public class Permutations_46 {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if(list.contains(nums[i])) continue;
+            if(list.contains(nums[i])) continue;    // O(n)
             list.add(nums[i]);
             helper(res, list, nums);
             list.remove(list.size() - 1);
         }
     }
+
+
+    /**
+     * 第二种方法： 规避掉list.contains(nums[i])而增加的O(n)的时间复杂度
+     * time: O(n!)
+     * space: O(n)
+     */
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0) return res;
+        helper2(res, 0, nums);
+        return res;
+    }
+
+    private void helper2(List<List<Integer>> res, int start, int[] nums) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            res.add(new ArrayList<>(list));
+            return ;
+        }
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            helper2(res, start+1, nums);
+            swap(nums, start, i);
+        }
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+
 
 }
