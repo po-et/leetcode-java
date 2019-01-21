e10487ca47d9365b77437621b5ef4c3aa469307e
poet <ispoetmail@gmail.com>
2019-01-21 16:53:02 +0800

    update ValidParentheses_20, add method 2

--- src/main/java/com/home/leetcode/ValidParentheses_20.java
+++ src/main/java/com/home/leetcode/ValidParentheses_20.java
@@ -37,6 +37,29 @@ public class ValidParentheses_20 {
         return stack.isEmpty();
     }
 
+
+    public static boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(')
+                    return false;
+                if (c == ']' && topChar != '[')
+                    return false;
+                if (c == '}' && topChar != '{')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
     public static void main(String[] args) {
         String s = "([)]{}";
         System.out.println(isValid(s));
