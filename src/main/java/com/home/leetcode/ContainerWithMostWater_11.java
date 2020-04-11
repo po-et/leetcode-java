265efaa6ea22484718742eb63ba59ea2aff4b357
Poet <ispoetmail@gmail.com>
2020-04-12 00:22:41 +0800

    review: ContainerWithMostWater_11.java

--- src/main/java/com/home/leetcode/ContainerWithMostWater_11.java
+++ src/main/java/com/home/leetcode/ContainerWithMostWater_11.java
@@ -1,17 +1,22 @@
 package com.home.leetcode;
 
 /**
+ * Example:
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ *
+ * @url https://leetcode.com/problems/container-with-most-water/
  * Created by Poet on 2019-01-10.
  */
 public class ContainerWithMostWater_11 {
 
     /**
-     * time: O(n)
-     * space: O(1)
+     * time complexity: O(n)
+     * space complexity: O(1)
      */
     public int maxArea(int[] height) {
         int res = 0;
-        int l = 0, r = height.length - 1;
+        int l   = 0, r = height.length - 1;
         while (l < r) {
             res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
             if (height[l] < height[r]) {
