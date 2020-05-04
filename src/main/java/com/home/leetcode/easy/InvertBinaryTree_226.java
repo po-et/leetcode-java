aa2030f16400daae8d7e80a08919f0a69e53e8ae
Poet <ispoetmail@gmail.com>
2020-05-04 14:17:20 +0800

    add(二叉树): [翻转] InvertBinaryTree_226

---
+++ src/main/java/com/home/leetcode/easy/InvertBinaryTree_226.java
@@ -0,0 +1,54 @@
+package com.home.leetcode.easy;
+
+/**
+ * Invert a binary tree.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ *
+ * Output:
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * @see <a href = "https://leetcode.com/problems/invert-binary-tree/" />
+ * @author Poet
+ * @date 2020/5/4
+ */
+public class InvertBinaryTree_226 {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+        root.right = leftNode;
+        root.left = rightNode;
+        return root;
+    }
+}
