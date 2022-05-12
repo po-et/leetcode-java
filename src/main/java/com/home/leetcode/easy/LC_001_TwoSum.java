aa6b6ebc1126533c6430282100f1b370a0206e8d
Poet <ispoetmail@gmail.com>
2022-05-12 22:14:09 +0800

    review: LC_001_TwoSum

--- src/main/java/com/home/leetcode/easy/LC_001_TwoSum.java
+++ src/main/java/com/home/leetcode/easy/LC_001_TwoSum.java
@@ -4,14 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * 1. 两数之和
  *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *
  * Example:
- *
  * Given nums = [2, 7, 11, 15], target = 9,
- *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  *
@@ -19,17 +18,15 @@ import java.util.Map;
  * @author Poet
  * @date 2020/4/19
  */
-public class TwoSum_1 {
+public class LC_001_TwoSum {
 
     /**
      * time: O(n)
      * space: O(n)
-     * @param nums
-     * @param target
-     * @return
      */
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> record = new HashMap<>(nums.length);
+
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if(record.containsKey(complement)){
@@ -39,6 +36,25 @@ public class TwoSum_1 {
         }
 
         throw new IllegalArgumentException("no solution!");
+    }
+
+    /**
+     * time: O(n^2)
+     */
+    public int[] twoSum2(int[] nums, int target) {
+
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        throw new IllegalArgumentException("not exist solution.");
     }
 
 }
