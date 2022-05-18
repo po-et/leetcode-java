d1dd2b89c7f2d66cb3cdbe96fc6e2495b5914224
Poet <ispoetmail@gmail.com>
2022-05-18 10:04:36 +0800

    review: LC_007_ReverseInteger

--- src/main/java/com/home/leetcode/medium/LC_007_ReverseInteger.java
+++ src/main/java/com/home/leetcode/medium/LC_007_ReverseInteger.java
@@ -1,16 +1,17 @@
-package com.home.leetcode;
+package com.home.leetcode.medium;
 
 /**
- * reverse integer
- * <p>
+ * 7. Reverse Integer
+ *
  * corner case: 越界（int越界！）
  *
- * time: O(n)
- * space: O(1)
+ * Example 1:
+ * Input: x = 123
+ * Output: 321
  *
  * Created by Poet on 2019-01-08.
  */
-public class ReverseInteger_7 {
+public class LC_007_ReverseInteger {
 
     public static void main(String[] args) {
         System.out.println(reverse(12345));
@@ -19,13 +20,22 @@ public class ReverseInteger_7 {
         System.out.println(reverse(Integer.MIN_VALUE));
     }
 
+    /**
+     * time: O(log|x|)
+     * space: O(1)
+     */
     public static int reverse(int x) {
         long res = 0;
+
         while (x != 0) {
             res = res * 10 + x % 10;
             x /= 10;
-            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+                return 0;
+            }
         }
+
         return (int) res;
     }
 }
