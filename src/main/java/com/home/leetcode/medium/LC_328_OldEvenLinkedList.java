e10f3119f3cfccf04478937d3589c4b515eb3911
Poet <ispoetmail@gmail.com>
2022-05-19 08:15:36 +0800

    review: LC_328_OldEvenLinkedList

--- src/main/java/com/home/leetcode/medium/LC_328_OldEvenLinkedList.java
+++ src/main/java/com/home/leetcode/medium/LC_328_OldEvenLinkedList.java
@@ -1,20 +1,21 @@
 package com.home.leetcode.medium;
 
 /**
+ * 328. Odd Even Linked List (奇偶链表)
+ *
  * Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
  *
  * You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
  *
  * Example 1:
- *
  * Input: 1->2->3->4->5->NULL
  * Output: 1->3->5->2->4->NULL
  *
- * @see <a href="https://leetcode.com/problems/odd-even-linked-list/" />
+ * @see <a href="https://leetcode.cn/problems/odd-even-linked-list/" />
  * @author Poet
  * @date 2020/4/28
  */
-public class OldEvenLinkedList_328 {
+public class LC_328_OldEvenLinkedList {
 
     public class ListNode {
         int val;
@@ -23,6 +24,36 @@ public class OldEvenLinkedList_328 {
     }
 
     public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = new ListNode(-1);
+        ListNode oddHead = odd;
+        ListNode even = new ListNode(-1);
+        ListNode evenHead = even;
+        int index = 1;
+
+        while (head != null) {
+            if (isOdd(index)) {
+                odd.next = head;
+                odd = odd.next;
+            } else {
+                even.next = head;
+                even = even.next;
+            }
+
+            index++;
+            head = head.next;
+        }
+
+        even.next = null;
+        odd.next = evenHead.next;
+
+        return oddHead.next;
+    }
+
+    public ListNode oddEvenList2(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
@@ -42,5 +73,9 @@ public class OldEvenLinkedList_328 {
         }
 
         return head;
+    }
+
+    private boolean isOdd(int num) {
+        return num % 2 != 0;
     }
 }
