0be769f5c7e8f77dab3be649da6904ade059d667
Poet <ispoetmail@gmail.com>
2022-05-21 09:08:15 +0800

    review: LC_061_RotateList

--- src/main/java/com/home/leetcode/medium/LC_061_RotateList.java
+++ src/main/java/com/home/leetcode/medium/LC_061_RotateList.java
@@ -1,6 +1,8 @@
 package com.home.leetcode.medium;
 
 /**
+ * 61. Rotate List
+ *
  * Given a linked list, rotate the list to the right by k places, where k is non-negative.
  *
  * Example 1:
@@ -12,23 +14,24 @@ package com.home.leetcode.medium;
  * rotate 1 steps to the right: 5->1->2->3->4->NULL
  * rotate 2 steps to the right: 4->5->1->2->3->NULL
  *
- * @see <a href = "https://leetcode.com/problems/rotate-list/" />
+ * @see <a href = "https://leetcode.cn/problems/rotate-list/" />
  * @author Poet
  * @date 2020/5/2
  */
-public class RotateList_61 {
+public class LC_061_RotateList {
 
     public class ListNode {
         int val;
         ListNode next;
-        ListNode(int x) { val = x; }
+
+        ListNode(int x) {val = x;}
     }
 
     /**
      * 双指针
      */
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null ) return head;
+        if (head == null || head.next == null) return head;
 
         ListNode curr = head;
         int len = 0;
@@ -66,12 +69,12 @@ public class RotateList_61 {
 
     /**
      * 方法二：(tricky 推荐！)
-     *
+     * <p>
      * 不用快慢指针，一个指针就够了，原理是先遍历整个链表获得链表长度n，然后此时把链表头和尾链接起来，
      * 在往后走n - k % n个节点就到达新链表的头结点前一个点，这时断开链表即可
      */
     public ListNode rotateRight2(ListNode head, int k) {
-        if(head == null || head.next == null) return head;
+        if (head == null || head.next == null) return head;
 
         ListNode curr = head;
         int n = 1;
