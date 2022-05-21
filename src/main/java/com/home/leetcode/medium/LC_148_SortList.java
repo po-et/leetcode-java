71d0f8e2e21ad36a1fc19a9d7fe9ddaf9a591936
Poet <ispoetmail@gmail.com>
2022-05-21 08:23:49 +0800

    review: LC_148_SortList

--- src/main/java/com/home/leetcode/medium/LC_148_SortList.java
+++ src/main/java/com/home/leetcode/medium/LC_148_SortList.java
@@ -1,6 +1,8 @@
 package com.home.leetcode.medium;
 
 /**
+ * 148. Sort List
+ *
  * Sort a linked list in O(n log n) time using constant space complexity.
  *
  * Example 1:
@@ -8,11 +10,11 @@ package com.home.leetcode.medium;
  * Input: 4->2->1->3
  * Output: 1->2->3->4
  *
- * @see <a href="https://leetcode.com/problems/sort-list/" />
+ * @see <a href="https://leetcode.cn/problems/sort-list/" />
  * @author Poet
  * @date 2020/5/2
  */
-public class SortList_148 {
+public class LC_148_SortList {
 
     public class ListNode {
         int val;
@@ -33,6 +35,7 @@ public class SortList_148 {
         if (head == null || head.next == null) {
             return head;
         }
+
         ListNode slow = head, fast = head, prev = null;
         //注意，这里不能写成while (fast.next != null && fast.next.next != null)，因为，在只有两个node的情况下fast和slow指针还是需要向下走，否则无法分解成一个node.
         while (fast != null && fast.next != null) {
@@ -40,18 +43,19 @@ public class SortList_148 {
             slow = slow.next;
             fast = fast.next.next;
         }
-        //note: 这里需要一个pre指针来保存slow的上一个node，因为需要把上一个node的next设为null
+        //【重点】: 这里需要一个pre指针来保存slow的上一个node，因为需要把上一个node的next设为null
         prev.next = null;
 
         ListNode l1 = sortList(head);
         ListNode l2 = sortList(slow);
+
         //merge的时候，参数是两个sort好的linkedList的head，需要使用sortList的返回值。
         return merge(l1, l2);
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
-        if(l1==null) return l2;
-        if(l2==null) return l1;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
         if (l1.val < l2.val) {
             l1.next = merge(l1.next, l2);
             return l1;
@@ -59,6 +63,28 @@ public class SortList_148 {
             l2.next = merge(l2.next, l1);
             return l2;
         }
+    }
+
+    private ListNode merge2(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) {
+            curr.next = l1;
+        }
+        if (l2 != null) {
+            curr.next = l2;
+        }
+        return dummyHead.next;
     }
 
 }
