9d8657de3980c6356def74ceb96190e62a3fe465
Poet <ispoetmail@gmail.com>
2022-05-21 08:57:07 +0800

    review: LC_019_RemoveNthNodeFromEndOfList

--- src/main/java/com/home/leetcode/medium/LC_019_RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/home/leetcode/medium/LC_019_RemoveNthNodeFromEndOfList.java
@@ -1,6 +1,8 @@
 package com.home.leetcode.medium;
 
 /**
+ * 19. Remove Nth Node From End of List
+ *
  * Given a linked list, remove the n-th node from the end of list and return its head.
  *
  * Example:
@@ -11,11 +13,11 @@ package com.home.leetcode.medium;
  * Note:
  * Given n will always be valid.
  *
- * @see <a href= "https://leetcode.com/problems/remove-nth-node-from-end-of-list/" />
+ * @see <a href= "https://leetcode.cn/problems/remove-nth-node-from-end-of-list/" />
  * @author Poet
  * @date 2020/5/2
  */
-public class RemoveNthNodeFromEndOfList_19 {
+public class LC_019_RemoveNthNodeFromEndOfList {
 
     public class ListNode {
         int val;
@@ -36,15 +38,18 @@ public class RemoveNthNodeFromEndOfList_19 {
         ListNode q = dummy;
 
         for (int i = 0; i < n + 1; i++) {
+//            assert q != null;
             q = q.next;
         }
         while (q != null) {
-            q=q.next;
-            p=p.next;
+            q = q.next;
+            p = p.next;
         }
+
         // remove
         p.next = p.next.next;
 
         return dummy.next;
     }
+
 }
