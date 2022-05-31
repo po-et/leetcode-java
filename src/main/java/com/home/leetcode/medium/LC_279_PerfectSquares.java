7a7fb9e100496b1cd909ee212a8ed3be852eacf4
Poet <ispoetmail@gmail.com>
2022-05-31 08:32:37 +0800

    review: LC_279_PerfectSquares

--- src/main/java/com/home/leetcode/medium/LC_279_PerfectSquares.java
+++ src/main/java/com/home/leetcode/medium/LC_279_PerfectSquares.java
@@ -9,11 +9,13 @@ package com.home.leetcode.medium;
  * with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
  *
  * Example 1:
+ *
  * Input: n = 12
  * Output: 3
  * Explanation: 12 = 4 + 4 + 4.
  *
  * Example 2:
+ *
  * Input: n = 13
  * Output: 2
  * Explanation: 13 = 4 + 9.
@@ -30,12 +32,14 @@ public class LC_279_PerfectSquares {
      */
     public int numSquares(int n) {
         int[] dp = new int[n + 1]; // 默认初始化值都为0
+
         for (int i = 1; i <= n; i++) {
             dp[i] = i; // 最坏的情况就是每次+1
             for (int j = 1; i - j * j >= 0; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 动态转移方程
             }
         }
+
         return dp[n];
     }
 
