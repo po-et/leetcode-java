389e3e725421d7febaa3c336a8659f586128d47f
Poet <ispoetmail@gmail.com>
2022-06-12 11:29:24 +0800

    new: LC_136_SingleNumber, LC_160_IntersectionOfTwoLinkedLists

---
+++ src/main/java/com/home/leetcode/easy/LC_136_SingleNumber.java
@@ -0,0 +1,70 @@
+package com.home.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 136. Single Number
+ * 
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ *
+ * Example 1:
+ * Input: nums = [2,2,1]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: nums = [4,1,2,1,2]
+ * Output: 4
+ *
+ * Example 3:
+ * Input: nums = [1]
+ * Output: 1
+ *
+ * @see <a href="https://leetcode.cn/problems/single-number" />
+ * @author Poet
+ * @date 2022/6/12
+ */
+public class LC_136_SingleNumber {
+
+    /**
+     * 方法一：位运算（异或）
+     *
+     * 异或运算有以下三个性质。
+     *
+     * 1、任何数和0做异或运算，结果仍然是原来的数，即 a⊕0=a。
+     * 2、任何数和其自身做异或运算，结果是 0，即 a⊕a=0。
+     * 3、异或运算满足交换律和结合律，即 a⊕b⊕a = b⊕a⊕a = b⊕(a⊕a) = b⊕0 = b。
+     *
+     * 官方题解：https://leetcode.cn/problems/single-number/solution/zhi-chu-xian-yi-ci-de-shu-zi-by-leetcode-solution/
+     *
+     * time:  O(n)
+     * space: O(1)
+     */
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single = single ^ num;
+        }
+        return single;
+    }
+
+    /**
+     * 方法二：哈希表
+     * @param nums
+     * @return
+     */
+    public int singleNumber_hashtable(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;  // no solution
+    }
+}
---
+++ src/main/java/com/home/leetcode/easy/LC_160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,71 @@
+package com.home.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 160. Intersection of Two Linked Lists （判断两个链表是否相交）
+ *
+ * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+ *
+ * @see <a href="https://leetcode.cn/problems/intersection-of-two-linked-lists" />
+ * @author Poet
+ * @date 2022/6/12
+ */
+public class LC_160_IntersectionOfTwoLinkedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * 方法一：哈希表
+     *
+     * time:  O(m+n) 其中 m 和 n 是分别是链表 headA 和 headB 的长度
+     * space: O(m)
+     */
+    public ListNode getIntersectionNode_hashTable(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+
+        Set<ListNode> seenSet = new HashSet<>();
+        while (curA != null) {
+            seenSet.add(curA);
+            curA = curA.next;
+        }
+
+        while (curB != null) {
+            if (seenSet.contains(curB)) {
+                return curB;
+            }
+            curB = curB.next;
+        }
+        return null;
+    }
+
+    /**
+     * 方法二：双指针（思路很有趣！）
+     *
+     * 使用双指针的方法，可以将空间复杂度降至 O(1)
+     *
+     * time:  O(m+n) 其中 m 和 n 是分别是链表 headA 和 headB 的长度
+     * space: O(1)
+     */
+    public ListNode getIntersectionNode_two_pointer(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
