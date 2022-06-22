b4f2cf5be632cefd562fb54dd4dbd6207a2c788f
yangbin26 <yangbin26@meituan.com>
2022-06-22 23:45:26 +0800

    offer(day18): Offer_55_I_MaximumDepthOfBinaryTree， Offer_55_II_BalancedBinaryTree

---
+++ src/main/java/com/home/jianzhi_offer/Offer_55_I_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,47 @@
+package com.home.jianzhi_offer;
+
+/**
+ * 剑指 Offer 55 - I. 二叉树的深度
+ *
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ *
+ * 例如：
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ * @see <a href="https://leetcode.cn/problems/er-cha-shu-de-shen-du-lcof/" />
+ * @author Poet
+ * @date 2022/6/22
+ */
+public class Offer_55_I_MaximumDepthOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {val = x;}
+    }
+
+
+    /**
+     * 方法一：深度优先搜索
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
