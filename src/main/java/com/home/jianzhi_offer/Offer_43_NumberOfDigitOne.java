a23bb068f554fbaaa2f0c0433873b5bbef9ec602
yangbin26 <yangbin26@meituan.com>
2022-07-05 21:20:34 +0800

    offer(day31): Offer_14_II_CuttingRope, Offer_43_NumberOfDigitOne, Offer_44_FindNthDigit

--- src/main/java/com/home/jianzhi_offer/Offer_43_NumberOfDigitOne.java
+++ src/main/java/com/home/jianzhi_offer/Offer_43_NumberOfDigitOne.java
@@ -6,38 +6,37 @@ package com.home.jianzhi_offer;
  * 输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
  *
  * 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+ *
  * 示例 1：
  * 输入：n = 12
  * 输出：5
  *
- * 链接：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof
+ * 示例 2：
+ * 输入：n = 13
+ * 输出：6
+ *
+ * @see <a href="https://leetcode.cn/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/" />
  * @author Poet
  * @date 2020/5/31
  */
 public class Offer_43_NumberOfDigitOne {
 
-
-    // 超出时间限制
-    // 入参：824883294
-    public static int countDigitOne(int n) {
-        int res = 0;
-        for (int i = 1; i <= n ; i++) {
-            res += stat(i);
+    /**
+     * 链接：https://leetcode.cn/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/mian-shi-ti-43-1n-zheng-shu-zhong-1-chu-xian-de-2/
+     */
+    public int countDigitOne(int n) {
+        int digit = 1, res = 0;
+        int high = n / 10, cur = n % 10, low = 0;
+        while (high != 0 || cur != 0) {
+            if (cur == 0) res += high * digit;
+            else if (cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
         }
         return res;
     }
 
-    private static int stat(int n) {
-        int res = 0;
-        while (n > 0) {
-            int mod = n % 10;
-            if(mod == 1) res++;
-            n = n / 10;
-        }
-        return res;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(countDigitOne(824883294));
-    }
 }
