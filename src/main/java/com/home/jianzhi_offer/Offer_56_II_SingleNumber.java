ec5c307ccb12d58a452c1e73c0d4cf7971835c1d
yangbin26 <yangbin26@meituan.com>
2022-07-05 14:56:31 +0800

    offer(day22): Offer_56_I_SingleNumber， Offer_56_II_SingleNumber

---
+++ src/main/java/com/home/jianzhi_offer/Offer_56_II_SingleNumber.java
@@ -0,0 +1,34 @@
+package com.home.jianzhi_offer;
+
+/**
+ * 剑指 Offer 56 - II. 数组中数字出现的次数 II
+ *
+ * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+ *
+ * 示例 1：
+ * 输入：nums = [3,4,3,3]
+ * 输出：4
+ *
+ * 示例 2：
+ * 输入：nums = [9,1,7,9,7,9,7]
+ * 输出：1
+ *
+ * @see <a href="https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/" />
+ * @author Poet
+ * @date 2022/7/5
+ */
+public class Offer_56_II_SingleNumber {
+
+    /**
+     * 位运算 + 有限状态自动机
+     */
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+
+}
