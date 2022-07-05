330b39f2c3d521c1ed87bee7156732fb4a4d6fc2
yangbin26 <yangbin26@meituan.com>
2022-07-05 20:46:44 +0800

    offer(day28): Offer_37_SerializeAndDeserializeBinaryTree, Offer_38_Permutation

--- src/main/java/com/home/jianzhi_offer/Offer_37_SerializeAndDeserializeBinaryTree.java
+++ src/main/java/com/home/jianzhi_offer/Offer_37_SerializeAndDeserializeBinaryTree.java
@@ -1,11 +1,16 @@
 package com.home.jianzhi_offer;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 面试题37. 序列化二叉树
  *
  * 请实现两个函数，分别用来序列化和反序列化二叉树。
  *
- * 示例: 
+ * 示例:
  * 你可以将以下二叉树：
  *
  *     1
@@ -16,11 +21,15 @@ package com.home.jianzhi_offer;
  *
  * 序列化为 "[1,2,3,null,null,4,5]"
  *
- * 链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof
+ * @see <a href="https://leetcode.cn/problems/xu-lie-hua-er-cha-shu-lcof/" />
  * @author Poet
  * @date 2020/5/31
  */
 public class Offer_37_SerializeAndDeserializeBinaryTree {
+
+    // Your Codec object will be instantiated and called as such:
+    // Codec codec = new Codec();
+    // codec.deserialize(codec.serialize(root));
 
     public class TreeNode {
         int val;
@@ -29,14 +38,44 @@ public class Offer_37_SerializeAndDeserializeBinaryTree {
         TreeNode(int val) { this.val = val; }
     }
 
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        return null;
-    }
+    /**
+     * 方法一：深度优先搜索
+     */
+    public class Codec {
+        public String serialize(TreeNode root) {
+            return rserialize(root, "");
+        }
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        return null;
+        public TreeNode deserialize(String data) {
+            String[] dataArray = data.split(",");
+            List<String> dataList = new LinkedList<String>(Arrays.asList(dataArray));
+            return rdeserialize(dataList);
+        }
+
+        public String rserialize(TreeNode root, String str) {
+            if (root == null) {
+                str += "None,";
+            } else {
+                str += root.val + ",";
+                str = rserialize(root.left, str);
+                str = rserialize(root.right, str);
+            }
+            return str;
+        }
+
+        public TreeNode rdeserialize(List<String> dataList) {
+            if (dataList.get(0).equals("None")) {
+                dataList.remove(0);
+                return null;
+            }
+
+            TreeNode root = new TreeNode(Integer.valueOf(dataList.get(0)));
+            dataList.remove(0);
+            root.left = rdeserialize(dataList);
+            root.right = rdeserialize(dataList);
+
+            return root;
+        }
     }
 
 }
