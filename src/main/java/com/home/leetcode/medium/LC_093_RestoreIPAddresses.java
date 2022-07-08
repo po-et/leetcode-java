2120ea87fd7f3c0a7f4f9a0b19d55e3f18f55585
Poet <ispoetmail@gmail.com>
2022-07-08 09:01:21 +0800

    new: code

---
+++ src/main/java/com/home/leetcode/medium/LC_093_RestoreIPAddresses.java
@@ -0,0 +1,32 @@
+package com.home.leetcode.medium;
+
+import java.util.List;
+
+/**
+ * 93. Restore IP Addresses
+ *
+ * A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255
+ * (inclusive) and cannot have leading zeros.
+ *
+ * Example 1:
+ *
+ * Input: s = "25525511135"
+ * Output: ["255.255.11.135","255.255.111.35"]
+ *
+ * Example 2:
+ *
+ * Input: s = "0000"
+ * Output: ["0.0.0.0"]
+ *
+ *
+ * @see <a href="https://leetcode.cn/problems/restore-ip-addresses/" />
+ * @author Poet
+ * @date 2022/5/28
+ */
+public class LC_093_RestoreIPAddresses {
+
+    public List<String> restoreIpAddresses(String s) {
+        return null;
+    }
+
+}
