b595e8494d3029e09d460fec154eef105269c0ef
Poet <ispoetmail@gmail.com>
2022-07-13 08:44:11 +0800

    everyday(07.13): LC_735_AsteroidCollision

---
+++ src/main/java/com/home/leetcode/medium/LC_735_AsteroidCollision.java
@@ -0,0 +1,61 @@
+package com.home.leetcode.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 735. Asteroid Collision
+ *
+ * We are given an array asteroids of integers representing asteroids in a row.
+ *
+ * For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+ *
+ * Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+ *
+ * Example 1:
+ * Input: asteroids = [5,10,-5]
+ * Output: [5,10]
+ * Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+ *
+ * Example 2:
+ * Input: asteroids = [8,-8]
+ * Output: []
+ * Explanation: The 8 and -8 collide exploding each other.
+ *
+ * Example 3:
+ * Input: asteroids = [10,2,-5]
+ * Output: [10]
+ * Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ *
+ * @see <a href="https://leetcode.cn/problems/asteroid-collision/" />
+ * @author Poet
+ * @date 2022/7/13
+ */
+public class LC_735_AsteroidCollision {
+
+    /**
+     * 方法一：栈模拟
+     */
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int aster : asteroids) {
+            boolean alive = true;
+            while (alive && aster < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                alive = stack.peek() < -aster; // aster 是否存在
+                if (stack.peek() <= -aster) {  // 栈顶行星爆炸
+                    stack.pop();
+                }
+            }
+            if (alive) {
+                stack.push(aster);
+            }
+        }
+        int size = stack.size();
+        int[] ans = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+
+}
