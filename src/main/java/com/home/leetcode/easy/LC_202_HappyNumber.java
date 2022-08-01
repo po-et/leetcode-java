8510bd25bf12d5ecfb8be225578887120455c30a
Poet <ispoetmail@gmail.com>
2022-08-02 00:02:33 +0800

    new: LC_54_SpiralMatrix

--- src/main/java/com/home/leetcode/easy/LC_202_HappyNumber.java
+++ src/main/java/com/home/leetcode/easy/LC_202_HappyNumber.java
@@ -4,27 +4,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 202. 快乐数
+ * 202. Happy Number (快乐数)
  *
- * Write an algorithm to determine if a number n is "happy".
+ * Write an algorithm to determine if a number n is happy.
  *
- * @see <a href = "https://leetcode.cn/problems/happy-number/" />
+ * A happy number is a number defined by the following process:
+ *     - Starting with any positive integer, replace the number by the sum of the squares of its digits.
+ *     - Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+ *           Those numbers for which this process ends in 1 are happy.
+ *     - Return true if n is a happy number, and false if not.
+ *
+ * Example 1:
+ * Input: n = 19
+ * Output: true
+ * Explanation:
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ *
+ * Example 2:
+ * Input: n = 2
+ * Output: false
+ *
+ * @see <a href="https://leetcode.cn/problems/happy-number/" />
  * @author Poet
  * @date 2020/4/18
  */
 public class LC_202_HappyNumber {
 
-    public static void main(String[] args) {
-        LC_202_HappyNumber solution = new LC_202_HappyNumber();
-//        int n = 19;
-        int n = 7;
-        System.out.println(solution.isHappy(n));
-    }
-
+    /**
+     * 方法一：用哈希集合检测循环
+     */
     public boolean isHappy(int n) {
-        n = Math.abs(n);
         Set<Integer> record = new HashSet<>();
-
+        n = Math.abs(n);
         int sum = n;
 
         while (true) {
@@ -44,11 +58,16 @@ public class LC_202_HappyNumber {
         while (n > 0) {
             int digit = n % 10;
             n = n / 10;
-            sum = sum + digit * digit;
+            sum += digit * digit;
         }
-
         return sum;
     }
 
+
+    public static void main(String[] args) {
+        LC_202_HappyNumber solution = new LC_202_HappyNumber();
+        System.out.println(solution.isHappy(7));
+        System.out.println(solution.isHappy(19));
+    }
 
 }
---
+++ src/main/java/com/home/leetcode/medium/LC_54_SpiralMatrix.java
@@ -0,0 +1,94 @@
+package com.home.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 54. Spiral Matrix (螺旋矩阵)
+ *
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ *
+ * Example 1:
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ *
+ * Example 2:
+ * Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * @see <a href="https://leetcode.cn/problems/spiral-matrix/" />
+ * @author Poet
+ * @date 2022/8/1
+ */
+public class LC_54_SpiralMatrix {
+
+    /**
+     * 方法一：模拟
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+
+        int rows = matrix.length, cols = matrix[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        int total = rows * cols;
+        int row = 0, col = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0;
+
+        for (int i = 0; i < total; i++) {
+            res.add(matrix[row][col]);
+            visited[row][col] = true;
+
+            int nextRow = row + directions[directionIndex][0], nextCol = col + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols || visited[nextRow][nextCol]) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            col += directions[directionIndex][1];
+        }
+
+        return res;
+    }
+
+
+
+//    public List<Integer> spiralOrder__(int[][] matrix) {
+//        List<Integer> res = new ArrayList<>();
+//        int direction = 1;
+//        int m = matrix.length;
+//        int n = matrix[0].length;
+//
+//        int i = 0;
+//        int j = 0;
+//
+//        while (i != j) {
+//            if (direction == 1) {
+//                while (j >= 0 && j < m) {
+//                    res.add(matrix[i][j++]);
+//                }
+//            }
+//            if (direction == 2) {
+//                while (i >= 0 && i < n) {
+//                    res.add(matrix[i++][j]);
+//                }
+//            }
+//            if (direction == 3) {
+//                while (i >= 0 && i < n) {
+//                    res.add(matrix[i][j--]);
+//                }
+//            }
+//            if (direction == 4) {
+//                while (i >= 0 && i < n) {
+//                    res.add(matrix[i--][j]);
+//                }
+//            }
+//
+//            direction = direction % 4;
+//        }
+//
+//        return res;
+//    }
+}
