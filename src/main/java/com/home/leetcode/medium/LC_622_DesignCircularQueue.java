c8714a2834db3aac8075f5a83da8b36eeeb7c676
Poet <ispoetmail@gmail.com>
2022-08-02 08:03:38 +0800

    everyday(08.02): LC_622_DesignCircularQueue

---
+++ src/main/java/com/home/leetcode/medium/LC_622_DesignCircularQueue.java
@@ -0,0 +1,100 @@
+package com.home.leetcode.medium;
+
+/**
+ * 622. Design Circular Queue (设计循环队列)
+ *
+ * Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+ *
+ * One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+ *
+ * Implementation the MyCircularQueue class:
+ *
+ * MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+ * int Front() Gets the front item from the queue. If the queue is empty, return -1.
+ * int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+ * boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+ * boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+ * boolean isEmpty() Checks whether the circular queue is empty or not.
+ * boolean isFull() Checks whether the circular queue is full or not.
+ * You must solve the problem without using the built-in queue data structure in your programming language.
+ *
+ * Example 1:
+ * Input
+ * ["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
+ * [[3], [1], [2], [3], [4], [], [], [], [4], []]
+ * Output
+ * [null, true, true, true, false, 3, true, true, true, 4]
+ *
+ * Explanation
+ * MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+ * myCircularQueue.enQueue(1); // return True
+ * myCircularQueue.enQueue(2); // return True
+ * myCircularQueue.enQueue(3); // return True
+ * myCircularQueue.enQueue(4); // return False
+ * myCircularQueue.Rear();     // return 3
+ * myCircularQueue.isFull();   // return True
+ * myCircularQueue.deQueue();  // return True
+ * myCircularQueue.enQueue(4); // return True
+ *
+ * @see <a href="https://leetcode.cn/problems/design-circular-queue/" />
+ * @author Poet
+ * @date 2022/8/2
+ */
+public class LC_622_DesignCircularQueue {
+
+    /**
+     * 方法一：数组
+     */
+    class MyCircularQueue {
+        private int[] elements; // 一个固定大小的数组，用于保存循环队列的元素
+        private int capacity;   // 循环队列的容量，即队列中最多可以容纳的元素数量
+        private int front;      // 队列首元素对应的数组的索引
+        private int rear;       // 队列尾元素对应的索引的下一个索引
+
+        public MyCircularQueue(int k) {
+            capacity = k + 1;
+            elements = new int[capacity];
+            rear = front = 0;
+        }
+
+        public boolean enQueue(int value) {
+            if (isFull()) {
+                return false;
+            }
+            elements[rear] = value;
+            rear = (rear + 1) % capacity;
+            return true;
+        }
+
+        public boolean deQueue() {
+            if (isEmpty()) {
+                return false;
+            }
+            front = (front + 1) % capacity;
+            return true;
+        }
+
+        public int Front() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return elements[front];
+        }
+
+        public int Rear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return elements[(rear - 1 + capacity) % capacity];
+        }
+
+        public boolean isEmpty() {
+            return rear == front;
+        }
+
+        public boolean isFull() {
+            return ((rear + 1) % capacity) == front;
+        }
+    }
+
+}
