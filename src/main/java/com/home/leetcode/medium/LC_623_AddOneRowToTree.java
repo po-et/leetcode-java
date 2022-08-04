b62253342c8b57087b5337b1cbe3c2d8eafee04c
Poet <ispoetmail@gmail.com>
2022-08-05 07:44:32 +0800

    everyday(08.05): LC_623_AddOneRowToTree

---
+++ src/main/java/com/home/leetcode/medium/LC_623_AddOneRowToTree.java
@@ -0,0 +1,70 @@
+package com.home.leetcode.medium;
+
+/**
+ * 623. Add One Row to Tree (在二叉树中增加一行)
+ *
+ * Given the root of a binary tree and two integers val and depth, add a row of nodes with value val at the given depth depth.
+ *
+ * Note that the root node is at depth 1.
+ *
+ * The adding rule is:
+ *
+ * Given the integer depth, for each not null tree node cur at the depth depth - 1, create two tree nodes with value val as cur's left subtree root and right subtree root.
+ * cur's original left subtree should be the left subtree of the new left subtree root.
+ * cur's original right subtree should be the right subtree of the new right subtree root.
+ * If depth == 1 that means there is no depth depth - 1 at all, then create a tree node with value val as the new root of the whole original tree, and the original tree is the new root's left subtree.
+ *
+ * Example 1:
+ * Input: root = [4,2,6,3,1,5], val = 1, depth = 2
+ * Output: [4,1,1,2,null,null,6,3,1,5]
+ *
+ * Example 2:
+ * Input: root = [4,2,null,3,1], val = 1, depth = 3
+ * Output: [4,2,null,1,1,3,null,null,1]
+ *
+ * @see <a href="https://leetcode.cn/problems/add-one-row-to-tree" />
+ * @author Poet
+ * @date 2022/8/5
+ */
+public class LC_623_AddOneRowToTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 方法一：深度优先搜索
+     *
+     * time:  O(n) 其中 n 为输入的树的节点数。最坏情况下，需要遍历整棵树
+     * space: O(n) 递归的深度最多为 O(n)
+     */
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+            return root;
+        }
+
+        // 当  depth 大于 2 时，需要继续递归往下层搜索，并将 depth 减去 1，直到搜索到 depth 为 2
+        if (depth > 2) {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+}
