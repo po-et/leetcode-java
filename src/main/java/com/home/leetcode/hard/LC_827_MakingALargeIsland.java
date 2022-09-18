a787b56a2a3829ed16713bb847dc218f7b97ffaa
yangbin26 <yangbin26@meituan.com>
2022-09-18 11:14:50 +0800

    everyday(09.18): LC_827_MakingALargeIsland

---
+++ src/main/java/com/home/leetcode/hard/LC_827_MakingALargeIsland.java
@@ -0,0 +1,94 @@
+package com.home.leetcode.hard;
+
+import java.util.*;
+
+/**
+ * 827. Making A Large Island (最大人工岛)
+ *
+ * You are given an n x n binary matrix grid. You are allowed to change at most one 0 to be 1.
+ *
+ * Return the size of the largest island in grid after applying this operation.
+ *
+ * An island is a 4-directionally connected group of 1s.
+ *
+ * Example 1:
+ * Input: grid = [[1,0],[0,1]]
+ * Output: 3
+ * Explanation: Change one 0 to 1 and connect two 1s, then we get an island with area = 3.
+ *
+ * Example 2:
+ * Input: grid = [[1,1],[1,0]]
+ * Output: 4
+ * Explanation: Change the 0 to 1 and make the island bigger, only one island with area = 4.
+ *
+ * Example 3:
+ * Input: grid = [[1,1],[1,1]]
+ * Output: 4
+ * Explanation: Can't change any 0 to 1, only one island with area = 4.
+ *
+ * @see <a href="https://leetcode.cn/problems/making-a-large-island/" />
+ * @author Poet
+ * @date 2022/9/18
+ */
+public class LC_827_MakingALargeIsland {
+
+    // --- 直接参考答案
+
+    /**
+     * 方法一：标记岛屿 + 合并
+     */
+    static int[] d = {0, -1, 0, 1, 0};
+
+    public int largestIsland(int[][] grid) {
+        int n = grid.length;
+        int res = 0;
+        int[][] tag = new int[n][n];
+        Map<Integer, Integer> area = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && tag[i][j] == 0) {
+                    int t = i * n + j + 1;
+                    area.put(t, dfs(grid, i, j, tag, t));
+                    res = Math.max(res, area.get(t));
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    int z = 1;
+                    Set<Integer> connected = new HashSet<>();
+                    for (int k = 0; k < 4; k++) {
+                        int x = i + d[k], y = j + d[k + 1];
+                        if (!valid(n, x, y) || tag[x][y] == 0 || connected.contains(tag[x][y])) {
+                            continue;
+                        }
+                        z += area.get(tag[x][y]);
+                        connected.add(tag[x][y]);
+                    }
+                    res = Math.max(res, z);
+                }
+            }
+        }
+        return res;
+    }
+
+    public int dfs(int[][] grid, int x, int y, int[][] tag, int t) {
+        int n = grid.length, res = 1;
+        tag[x][y] = t;
+        for (int i = 0; i < 4; i++) {
+            int x1 = x + d[i], y1 = y + d[i + 1];
+            if (valid(n, x1, y1) && grid[x1][y1] == 1 && tag[x1][y1] == 0) {
+                res += dfs(grid, x1, y1, tag, t);
+            }
+        }
+        return res;
+    }
+
+    public boolean valid(int n, int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
+
+}
