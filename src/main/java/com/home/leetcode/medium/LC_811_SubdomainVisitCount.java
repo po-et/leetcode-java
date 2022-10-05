83c91e3b61867285348487925bcbabcc1b742b9f
Poet <ispoetmail@gmail.com>
2022-10-05 09:01:18 +0800

    everyday(10.05): LC_811_SubdomainVisitCount

---
+++ src/main/java/com/home/leetcode/medium/LC_811_SubdomainVisitCount.java
@@ -0,0 +1,68 @@
+package com.home.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 811. Subdomain Visit Count (子域名访问计数)
+ *
+ * A website domain "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com" and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
+ *
+ * A count-paired domain is a domain that has one of the two formats "rep d1.d2.d3" or "rep d1.d2" where rep is the number of visits to the domain and d1.d2.d3 is the domain itself.
+ *
+ * For example, "9001 discuss.leetcode.com" is a count-paired domain that indicates that discuss.leetcode.com was visited 9001 times.
+ * Given an array of count-paired domains cpdomains, return an array of the count-paired domains of each subdomain in the input. You may return the answer in any order.
+ *
+ * Example 1:
+ * Input: cpdomains = ["9001 discuss.leetcode.com"]
+ * Output: ["9001 leetcode.com","9001 discuss.leetcode.com","9001 com"]
+ * Explanation: We only have one website domain: "discuss.leetcode.com".
+ * As discussed above, the subdomain "leetcode.com" and "com" will also be visited. So they will all be visited 9001 times.
+ *
+ * Example 2:
+ * Input: cpdomains = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+ * Output: ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+ * Explanation: We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 5 times.
+ * For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+ *
+ * @see <a href="https://leetcode.cn/problems/subdomain-visit-count/" />
+ * @author Poet
+ * @date 2022/10/5
+ */
+public class LC_811_SubdomainVisitCount {
+
+    /**
+     * 方法一：哈希表
+     *
+     * time:  O(L)
+     * space: O(L)
+     */
+    public List<String> subdomainVisits(String[] cpdomains) {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> counts = new HashMap<>();
+
+        for (String cpdomain : cpdomains) {
+            int space = cpdomain.indexOf(' ');
+            int count = Integer.parseInt(cpdomain.substring(0, space));
+            String domain = cpdomain.substring(space + 1);
+            counts.put(domain, counts.getOrDefault(domain, 0) + count);
+
+            for (int i = 0; i < domain.length(); i++) {
+                if (domain.charAt(i) == '.') {
+                    String subdomain = domain.substring(i + 1);
+                    counts.put(subdomain, counts.getOrDefault(subdomain, 0) + count);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            String subdomain = entry.getKey();
+            int count = entry.getValue();
+            res.add(count + " " + subdomain);
+        }
+        return res;
+    }
+
+}
