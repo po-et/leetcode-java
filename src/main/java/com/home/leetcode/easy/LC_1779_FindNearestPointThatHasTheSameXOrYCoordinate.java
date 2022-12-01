e063ebe8f1699597056beef6c34610e35801d678
Poet <ispoetmail@gmail.com>
2022-12-01 08:45:54 +0800

    everyday(12.01): LC_1779_FindNearestPointThatHasTheSameXOrYCoordinate

---
+++ src/main/java/com/home/leetcode/easy/LC_1779_FindNearestPointThatHasTheSameXOrYCoordinate.java
@@ -0,0 +1,61 @@
+package com.home.leetcode.easy;
+
+/**
+ * 1779. Find Nearest Point That Has the Same X or Y Coordinate (找到最近的有相同 X 或 Y 坐标的点)
+ *
+ * You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y).
+ * You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi).
+ * A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
+ *
+ * Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location.
+ * If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
+ *
+ * The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
+ *
+ * Example 1:
+ * Input: x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
+ * Output: 2
+ * Explanation: Of all the points, only [3,1], [2,4] and [4,4] are valid. Of the valid points, [2,4] and [4,4] have the smallest Manhattan distance from your current location, with a distance of 1. [2,4] has the smallest index, so return 2.
+ *
+ * Example 2:
+ * Input: x = 3, y = 4, points = [[3,4]]
+ * Output: 0
+ * Explanation: The answer is allowed to be on the same location as your current location.
+ *
+ * Example 3:
+ * Input: x = 3, y = 4, points = [[2,3]]
+ * Output: -1
+ * Explanation: There are no valid points.
+ *
+ * @see <a href="https://leetcode.cn/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/" />
+ * @author Poet
+ * @date 2022/12/1
+ */
+public class LC_1779_FindNearestPointThatHasTheSameXOrYCoordinate {
+
+    /**
+     * 方法一：枚举所有的点
+     */
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int n = points.length;
+        int minDistance = Integer.MAX_VALUE;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            int px = points[i][0], py = points[i][1];
+            if (x == px) {
+                int dist = Math.abs(y - py);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    res = i;
+                }
+            } else if (y == py) {
+                int dist = Math.abs(x - px);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
+}
