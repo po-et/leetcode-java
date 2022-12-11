d40bf18376b11cdfbfa07f5ca813754f07eb63bf
Poet <ispoetmail@gmail.com>
2022-12-11 11:22:30 +0800

    review

--- src/main/java/com/home/leetcode/easy/LC_088_MergeSortedArray.java
+++ src/main/java/com/home/leetcode/easy/LC_088_MergeSortedArray.java
@@ -1,13 +1,34 @@
 package com.home.leetcode.easy;
 
 /**
- * 88. 合并两个有序数组
+ * 88. Merge Sorted Array （合并两个有序数组）
  *
- * 给你两个按 非递减顺序 排列的整数数组nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
- * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ * Example 1:
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ *
+ * Example 2:
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * Explanation: The arrays we are merging are [1] and [].
+ * The result of the merge is [1].
+ *
+ * Example 3:
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Output: [1]
+ * Explanation: The arrays we are merging are [] and [1].
+ * The result of the merge is [1].
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
  *
  * @see <a href="https://leetcode.cn/problems/merge-sorted-array" />
- *
  * @author Poet
  * @date 2022/5/12
  */
@@ -15,11 +36,11 @@ public class LC_088_MergeSortedArray {
 
     /**
      * 方法二：双指针
-     * time: O(m+n)
+     *
+     * time:  O(m+n)
      * space: O(m+n)
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
         int[] res = new int[m + n];
         int index = 0;
 
@@ -28,7 +49,7 @@ public class LC_088_MergeSortedArray {
             if (nums1[i] <= nums2[j]) {
                 res[index++] = nums1[i];
                 i++;
-            } else { // nums1[i] > nums2[j]
+            } else {    // nums1[i] > nums2[j]
                 res[index++] = nums2[j];
                 j++;
             }
@@ -44,6 +65,52 @@ public class LC_088_MergeSortedArray {
 
         for (int k = 0; k < res.length; k++) {
             nums1[k] = res[k];
+        }
+    }
+
+    /**
+     * 从后往前的原地归并
+     */
+    public static void merge_1(int[] nums1, int m, int[] nums2, int n) {
+        int index = m + n - 1;
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] > nums2[n - 1])
+                nums1[index--] = nums1[--m];
+            else
+                nums1[index--] = nums2[--n];
+        }
+
+        while (m > 0) {
+            nums1[index--] = nums1[--m];
+        }
+        while (n > 0) {
+            nums1[index--] = nums2[--n];
+        }
+    }
+
+
+    /**
+     * 开辟O(m)空间
+     */
+    public static void merge_2(int[] nums1, int m, int[] nums2, int n) {
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++)
+            arr[i] = nums1[i];
+
+        int i = 0, j = 0;
+        int index = 0;
+        while (i < m && j < n) {
+            if(arr[i] <= nums2[j])
+                nums1[index++] = arr[i++];
+            else
+                nums1[index++] = nums2[j++];
+        }
+
+        while (i < m) {
+            nums1[index++] = arr[i++];
+        }
+        while (j < n) {
+            nums1[index++] = nums2[j++];
         }
     }
 }
