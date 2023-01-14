c2e2532ec4ec81d8f315e0342ee9d7f865aa2ae9
poet <ispoetmail@gmail.com>
2023-01-14 23:09:50 +0800

    Everyday(01.14): LC_1819_NumberOfDifferentSubsequencesGCDs

---
+++ src/main/java/com/home/leetcode/hard/LC_1819_NumberOfDifferentSubsequencesGCDs.java
@@ -0,0 +1,80 @@
+package com.home.leetcode.hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 1819. Number of Different Subsequences GCDs (序列中不同最大公约数的数目)
+ *
+ * You are given an array nums that consists of positive integers.
+ *
+ * The GCD of a sequence of numbers is defined as the greatest integer that divides all the numbers in the sequence evenly.
+ *
+ * For example, the GCD of the sequence [4,6,16] is 2.
+ * A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+ *
+ * For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+ * Return the number of different GCDs among all non-empty subsequences of nums.
+ *
+ * Example 1:
+ * Input: nums = [6,10,3]
+ * Output: 5
+ * Explanation: The figure shows all the non-empty subsequences and their GCDs.
+ * The different GCDs are 6, 10, 3, 2, and 1.
+ *
+ * Example 2:
+ * Input: nums = [5,15,40,5,6]
+ * Output: 7
+ *
+ * @see <a href="https://leetcode.cn/problems/number-of-different-subsequences-gcds/" />
+ * @author bin.yang
+ * @date 2023/1/14
+ */
+public class LC_1819_NumberOfDifferentSubsequencesGCDs {
+
+    // 参考答案
+
+    /**
+     * 方法一：枚举
+     */
+    public int countDifferentSubsequenceGCDs(int[] nums) {
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        boolean[] occured = new boolean[maxVal + 1];
+        for (int num : nums) {
+            occured[num] = true;
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= maxVal; i++) {
+            int subGcd = 0;
+            for (int j = i; j <= maxVal; j += i) {
+                if (occured[j]) {
+                    if (subGcd == 0) {
+                        subGcd = j;
+                    } else {
+                        subGcd = gcd(subGcd, j);
+                    }
+                    if (subGcd == i) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
+    }
+
+}
