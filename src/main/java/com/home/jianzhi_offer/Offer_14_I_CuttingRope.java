ee524581dbe7c566e6396cb2a7b5566c400ec8ba
poet <ispoetmail@gmail.com>
2023-01-18 21:42:32 +0800

    review: LC_343_IntegerBreak

--- src/main/java/com/home/jianzhi_offer/Offer_14_I_CuttingRope.java
+++ src/main/java/com/home/jianzhi_offer/Offer_14_I_CuttingRope.java
@@ -5,15 +5,16 @@ import com.home.leetcode.medium.LC_343_IntegerBreak;
 /**
  * 面试题14- I. 剪绳子
  *
- * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。
+ *
+ * 请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
  *
  * 示例 1：
- *
  * 输入: 2
  * 输出: 1
  * 解释: 2 = 1 + 1, 1 × 1 = 1
  *
- * 注意：本题与主站 343 题相同：https://leetcode-cn.com/problems/integer-break/
+ * 注意：本题与主站 343 题相同：https://leetcode.cn/problems/integer-break/
  * {@link LC_343_IntegerBreak}
  *
  * @see <a href="https://leetcode.cn/problems/jian-sheng-zi-lcof/" />
