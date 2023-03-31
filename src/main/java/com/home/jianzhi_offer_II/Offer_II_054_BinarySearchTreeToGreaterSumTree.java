a76e86d117b69ba4f2ee7a4b162b601a63700dc4
Poet <ispoetmail@gmail.com>
2023-03-31 10:57:10 +0800

    Offer(03.30)[day 18]: Offer_II_053_InorderSuccessorInBST, Offer_II_054_BinarySearchTreeToGreaterSumTree, Offer_II_055_BinarySearchTreeIterator

---
+++ src/main/java/com/home/jianzhi_offer_II/Offer_II_054_BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,70 @@
+package com.home.jianzhi_offer_II;
+
+/**
+ * 剑指 Offer II 054. 所有大于等于节点的值之和
+ *
+ * 给定一个二叉搜索树，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+ *
+ * 提醒一下，二叉搜索树满足下列约束条件：
+ *
+ * 节点的左子树仅包含键 小于 节点键的节点。
+ * 节点的右子树仅包含键 大于 节点键的节点。
+ * 左右子树也必须是二叉搜索树。
+ *
+ * 示例 1：
+ * 输入：root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+ * 输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ *
+ * 示例 2：
+ * 输入：root = [0,null,1]
+ * 输出：[1,null,1]
+ *
+ * 示例 3：
+ * 输入：root = [1,0,2]
+ * 输出：[3,3,2]
+ *
+ * 示例 4：
+ * 输入：root = [3,2,4,1]
+ * 输出：[7,9,4,10]
+ *
+ * 注意：
+ * 本题与主站 538 题相同： https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ * 本题与主站 1038 题相同：https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/
+ *
+ * @see <a href="https://leetcode.cn/problems/w6cpku/" />
+ * @author Poet
+ * @date 2023/3/30
+ */
+public class Offer_II_054_BinarySearchTreeToGreaterSumTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 方法一：反序中序遍历
+     */
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+
+        return root;
+    }
+}
---
+++ src/main/java/com/home/jianzhi_offer_II/Offer_II_055_BinarySearchTreeIterator.java
@@ -0,0 +1,132 @@
+package com.home.jianzhi_offer_II;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 剑指 Offer II 055. 二叉搜索树迭代器
+ *
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ *
+ * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+ * int next()将指针向右移动，然后返回指针处的数字。
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+ *
+ * 可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ *
+ * 示例：
+ * 输入
+ * inputs = ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+ * inputs = [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+ * 输出
+ * [null, 3, 7, true, 9, true, 15, true, 20, false]
+ *
+ * 解释
+ * BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+ * bSTIterator.next();    // 返回 3
+ * bSTIterator.next();    // 返回 7
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 9
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 15
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 20
+ * bSTIterator.hasNext(); // 返回 False
+ *
+ * 进阶：
+ * 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h 是树的高度。
+ *
+ * 注意：本题与主站 173 题相同： https://leetcode-cn.com/problems/binary-search-tree-iterator/
+ *
+ * @see <a href="https://leetcode.cn/problems/kTOapQ/" />
+ * @author Poet
+ * @date 2023/3/30
+ */
+public class Offer_II_055_BinarySearchTreeIterator {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 方法一：转换为中序遍历的列表
+     */
+    class BSTIterator {
+
+        private int index;
+        private List<Integer> inOrderList;
+
+        public BSTIterator(TreeNode root) {
+            index = 0;
+            inOrderList = new ArrayList<>();
+            inOrderTraverse(root);
+        }
+
+        public int next() {
+            return inOrderList.get(index++);
+        }
+
+        public boolean hasNext() {
+            return index < inOrderList.size();
+        }
+
+        private void inOrderTraverse(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            inOrderTraverse(root.left);
+            inOrderList.add(root.val);
+            inOrderTraverse(root.right);
+        }
+    }
+
+    /**
+     * 方法二：迭代
+     */
+    class BSTIterator_iteration {
+        TreeNode cur;
+        Deque<TreeNode> stack;
+
+        public BSTIterator_iteration(TreeNode root) {
+            cur = root;
+            stack = new LinkedList<>();
+        }
+
+        public int next() {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            int res = cur.val;
+            cur = cur.right;
+            return res;
+        }
+
+        public boolean hasNext() {
+            return cur != null || !stack.isEmpty();
+        }
+    }
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+}
