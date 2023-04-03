212d1004c806281d5c43580dc35eac5308d09221
Poet <ispoetmail@gmail.com>
2023-04-04 07:49:30 +0800

    Offer(04.04)[day 23]: Offer_II_068_SearchInsertPosition, Offer_II_069_PeakIndexInAMountainArray, Offer_II_070_SingleElementInASortedArray

---
+++ src/main/java/com/home/jianzhi_offer_II/Offer_II_068_SearchInsertPosition.java
@@ -0,0 +1,61 @@
+package com.home.jianzhi_offer_II;
+
+/**
+ * 剑指 Offer II 068. 查找插入位置
+ *
+ * 给定一个排序的整数数组 nums 和一个整数目标值 target ，请在数组中找到 target ，并返回其下标。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ *
+ * 示例 1:
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ *
+ * 示例 3:
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ *
+ * 示例 4:
+ * 输入: nums = [1,3,5,6], target = 0
+ * 输出: 0
+ *
+ * 示例 5:
+ * 输入: nums = [1], target = 0
+ * 输出: 0
+ *
+ * 注意：本题与主站 35 题相同： https://leetcode-cn.com/problems/search-insert-position/
+ *
+ * @see <a href="https://leetcode.cn/problems/N6YdxV/" />
+ * @author Poet
+ * @date 2023/4/4
+ */
+public class Offer_II_068_SearchInsertPosition {
+
+    // 题目本质为：「在一个有序数组中找第一个大于等于 target 的下标」
+
+    /**
+     * 方法一：二分查找
+     *
+     * time:  O(logN)
+     * space: O(1)
+     */
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int res = n;
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (target <= nums[mid]) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/home/jianzhi_offer_II/Offer_II_070_SingleElementInASortedArray.java
@@ -0,0 +1,70 @@
+package com.home.jianzhi_offer_II;
+
+/**
+ * 剑指 Offer II 070. 排序数组中只出现一次的数字
+ *
+ * 给定一个只包含整数的有序数组 nums ，每个元素都会出现两次，唯有一个数只会出现一次，请找出这个唯一的数字。
+ *
+ * 你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
+ *
+ * 示例 1:
+ * 输入: nums = [1,1,2,3,3,4,4,8,8]
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: nums =  [3,3,7,7,10,11,11]
+ * 输出: 10
+ *
+ * 注意：本题与主站 540 题相同：https://leetcode-cn.com/problems/single-element-in-a-sorted-array/
+ *
+ * @see <a href="https://leetcode.cn/problems/skFtm2/" />
+ * @author Poet
+ * @date 2023/4/4
+ */
+public class Offer_II_070_SingleElementInASortedArray {
+
+    /**
+     * 方法二：偶数下标的二分查找
+     *
+     * time:  O(logN)
+     * space: O(1)
+     */
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            // 确保mid是偶数
+            if (mid % 2 != 0) {
+                mid = mid - 1;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                l = mid + 2;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+
+    /**
+     * 滑动窗口（自己ac）
+     *
+     * time:  O(N)
+     */
+    public int singleNonDuplicate_bruteForce(int[] nums) {
+        int n = nums.length;
+        int res = nums[0];
+        int l = 0, r = 0;
+        while (l < n) {
+            while (r < n && nums[l] == nums[r]) {
+                r++;
+            }
+            if (r - l <= 1) {
+                return nums[l];
+            }
+            l = r;
+        }
+        return res;
+    }
+}
