8bd426a51e768197af00629b12f03882bc556608
Poet <ispoetmail@gmail.com>
2023-05-30 08:24:56 +0800

    Everyday(05.30): LC_1110_DeleteNodesAndReturnForest

---
+++ src/main/java/com/home/leetcode/medium/LC_1110_DeleteNodesAndReturnForest.java
@@ -0,0 +1,78 @@
+package com.home.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 1110. Delete Nodes And Return Forest
+ *
+ * Given the root of a binary tree, each node in the tree has a distinct value.
+ *
+ * After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+ *
+ * Return the roots of the trees in the remaining forest. You may return the result in any order.
+ *
+ * Example 1:
+ * Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+ * Output: [[1,2,null,4],[6],[7]]
+ *
+ * Example 2:
+ * Input: root = [1,2,4,null,3], to_delete = [3]
+ * Output: [[1,2,4]]
+ *
+ * @see <a href="https://leetcode.cn/problems/delete-nodes-and-return-forest/" />
+ * @author Poet
+ * @date 2023/5/30
+ */
+public class LC_1110_DeleteNodesAndReturnForest {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // 参考答案
+
+    /**
+     * 方法一：深度优先搜索
+     */
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> toDeleteSet = new HashSet<>();
+        for (int val : to_delete) {
+            toDeleteSet.add(val);
+        }
+
+        List<TreeNode> roots = new ArrayList<>();
+        dfs(root, true, toDeleteSet, roots);
+
+        return roots;
+    }
+
+    public TreeNode dfs(TreeNode node, boolean isRoot, Set<Integer> toDeleteSet, List<TreeNode> roots) {
+        if (node == null) {
+            return null;
+        }
+
+        boolean deleted = toDeleteSet.contains(node.val);
+        node.left = dfs(node.left, deleted, toDeleteSet, roots);
+        node.right = dfs(node.right, deleted, toDeleteSet, roots);
+        if (deleted) {
+            return null;
+        } else {
+            if (isRoot) {
+                roots.add(node);
+            }
+            return node;
+        }
+    }
+}
