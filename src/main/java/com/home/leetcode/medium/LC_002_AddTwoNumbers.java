c5a4562eaa2dc9f553e2f3603146f4f324cb04fe
Poet <ispoetmail@gmail.com>
2023-07-02 09:23:48 +0800

    Everyday(07.02): LC_002_AddTwoNumbers

--- src/main/java/com/home/leetcode/medium/LC_002_AddTwoNumbers.java
+++ src/main/java/com/home/leetcode/medium/LC_002_AddTwoNumbers.java
@@ -1,18 +1,25 @@
 package com.home.leetcode.medium;
 
 /**
- * 2. Add Two Numbers （链表中两数相加）
+ * 2. Add Two Numbers（链表中两数相加）
  *
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
  * order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
  *
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
  *
- * Example:
- *
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
- * Output: 7 -> 0 -> 8
+ * Example 1:
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
  * Explanation: 342 + 465 = 807.
+ *
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
  *
  * @see <a href="https://leetcode.cn/problems/add-two-numbers/" />
  * @author Poet
@@ -24,16 +31,49 @@ public class LC_002_AddTwoNumbers {
         int val;
         ListNode next;
 
-        ListNode(int x) { val = x; }
+        ListNode(int x) {val = x;}
     }
 
+    // 更好理解
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+
+
+    public ListNode addTwoNumbers_2(ListNode l1, ListNode l2) {
         if (l1 == null || l2 == null) {
             return null;
         }
 
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
 
         int tenth = 0;
         int digit = 0;
@@ -42,11 +82,11 @@ public class LC_002_AddTwoNumbers {
             digit = (l1.val + l2.val + tenth) % 10;
             tenth = (l1.val + l2.val + tenth) / 10;
 
-            curr.next = new ListNode(digit);
+            cur.next = new ListNode(digit);
 
             l1 = l1.next;
             l2 = l2.next;
-            curr = curr.next;
+            cur = cur.next;
 
             if (l1 == null && l2 == null) {
                 break;
@@ -61,11 +101,10 @@ public class LC_002_AddTwoNumbers {
 
         // 处理最后一位 >=10 的情况
         if (tenth != 0) {
-            curr.next = new ListNode(tenth);
+            cur.next = new ListNode(tenth);
         }
 
-        return dummy.next;
+        return dummyHead.next;
     }
-
 
 }
