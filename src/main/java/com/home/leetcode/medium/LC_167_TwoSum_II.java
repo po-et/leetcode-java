a57966dad9bf79ee745e2f34fa678cb94a191cc7
Poet <ispoetmail@gmail.com>
2023-07-08 07:44:28 +0800

    Everyday(07.08): LC_167_TwoSum_II

--- src/main/java/com/home/leetcode/medium/LC_167_TwoSum_II.java
+++ src/main/java/com/home/leetcode/medium/LC_167_TwoSum_II.java
@@ -3,12 +3,30 @@ package com.home.leetcode.medium;
 import java.util.Arrays;
 
 /**
- * 167. Two Sum II - Input Array Is Sorted （两数之和 II - 输入有序数组）
+ * 167. Two Sum II - Input Array Is Sorted
  *
- * Example:
- *  Input: numbers = [2,7,11,15], target = 9
- *  Output: [1,2]
- *  Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+ *
+ * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+ *
+ * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+ *
+ * Your solution must use only constant extra space.
+ *
+ * Example 1:
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+ *
+ * Example 2:
+ * Input: numbers = [2,3,4], target = 6
+ * Output: [1,3]
+ * Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+ *
+ * Example 3:
+ * Input: numbers = [-1,0], target = -1
+ * Output: [1,2]
+ * Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
  *
  * @see <a href="https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/" />
  * @author Poet
@@ -17,16 +35,20 @@ import java.util.Arrays;
 public class LC_167_TwoSum_II {
 
     /**
-     * 对撞指针
+     * 方法：双指针
+     *
+     * 解法的本质：从【缩减搜索空间】的角度思考这个解法
+     * @via: https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/solution/yi-zhang-tu-gao-su-ni-on-de-shuang-zhi-zhen-jie-fa/
      */
     public static int[] twoSum(int[] numbers, int target) {
         int l = 0;
         int r = numbers.length - 1;
 
         while (l < r) {
-            if (numbers[l] + numbers[r] == target) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) {
                 return new int[]{l + 1, r + 1};
-            } else if (numbers[l] + numbers[r] < target) {
+            } else if (sum < target) {
                 l++;
             } else {
                 r--;
