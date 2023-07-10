173b59a39f4cfcb1b8e75585e64ef704cb553130
Poet <ispoetmail@gmail.com>
2023-07-10 23:23:53 +0800

    Offer(07.10)[day 30]: Offer_II_088_MinCostClimbingStairs, Offer_II_089_HouseRobber, Offer_II_089_HouseRobber_ii

---
+++ src/main/java/com/home/jianzhi_offer_II/Offer_II_088_MinCostClimbingStairs.java
@@ -0,0 +1,44 @@
+package com.home.jianzhi_offer_II;
+
+/**
+ * 剑指 Offer II 088. 爬楼梯的最少成本
+ *
+ * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+ *
+ * 每当爬上一个阶梯都要花费对应的体力值，一旦支付了相应的体力值，就可以选择向上爬一个阶梯或者爬两个阶梯。
+ *
+ * 请找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+ *
+ * 示例 1：
+ * 输入：cost = [10, 15, 20]
+ * 输出：15
+ * 解释：最低花费是从 cost[1] 开始，然后走两步即可到阶梯顶，一共花费 15 。
+ *
+ * 示例 2：
+ * 输入：cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出：6
+ * 解释：最低花费方式是从 cost[0] 开始，逐个经过那些 1 ，跳过 cost[3] ，一共花费 6 。
+ *
+ * 注意：本题与主站 746 题相同： https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+ *
+ * @see <a href="https://leetcode.cn/problems/GzCJIP/" />
+ * @author Poet
+ * @date 2023/7/10
+ */
+public class Offer_II_088_MinCostClimbingStairs {
+
+    /**
+     * 方法：动态规划
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
+    }
+
+}
