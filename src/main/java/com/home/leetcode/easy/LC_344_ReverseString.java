d50e60a2e632f79f81bab08af68cae4e0283dffd
Poet <ispoetmail@gmail.com>
2023-08-07 08:38:13 +0800

    Everyday(08.07): LC_344_ReverseString

--- src/main/java/com/home/leetcode/easy/LC_344_ReverseString.java
+++ src/main/java/com/home/leetcode/easy/LC_344_ReverseString.java
@@ -1,7 +1,7 @@
 package com.home.leetcode.easy;
 
 /**
- * 344. 反转字符串
+ * 344. Reverse String（反转字符串）
  *
  * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
  * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
@@ -17,11 +17,12 @@ package com.home.leetcode.easy;
 public class LC_344_ReverseString {
 
     /**
-     * time: O(n)
+     * 方法：双指针
+     *
+     * time:  O(n)
      * space: O(1)
      */
     public void reverseString(char[] s) {
-
         int l = 0;
         int r = s.length - 1;
 
