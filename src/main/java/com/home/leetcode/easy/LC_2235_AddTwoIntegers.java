e069b38b54c7dc0169c2d27eb75184c714009773
Poet <ispoetmail@gmail.com>
2023-08-19 08:16:09 +0800

    Everyday(08.19): LC_2235_AddTwoIntegers

---
+++ src/main/java/com/home/leetcode/easy/LC_2235_AddTwoIntegers.java
@@ -0,0 +1,28 @@
+package com.home.leetcode.easy;
+
+/**
+ * 2235. Add Two Integers (两整数相加)
+ *
+ * Given two integers num1 and num2, return the sum of the two integers.
+ *
+ * Example 1:
+ * Input: num1 = 12, num2 = 5
+ * Output: 17
+ * Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+ *
+ * Example 2:
+ * Input: num1 = -10, num2 = 4
+ * Output: -6
+ * Explanation: num1 + num2 = -6, so -6 is returned.
+ *
+ * @see <a href="https://leetcode.cn/problems/add-two-integers/description/" />
+ * @author Poet
+ * @date 2023/8/19
+ */
+public class LC_2235_AddTwoIntegers {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
