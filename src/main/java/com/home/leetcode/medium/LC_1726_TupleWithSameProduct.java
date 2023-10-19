015dd7ea6f9e1ef1217d362330a93b98b5342143
Poet <ispoetmail@gmail.com>
2023-10-19 08:47:28 +0800

    Everyday(10.19): LC_1726_TupleWithSameProduct

---
+++ src/main/java/com/home/leetcode/medium/LC_1726_TupleWithSameProduct.java
@@ -0,0 +1,53 @@
+package com.home.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1726. Tuple with Same Product
+ *
+ * Given an array nums of distinct positive integers, return the number of tuples (a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+ *
+ * Example 1:
+ * Input: nums = [2,3,4,6]
+ * Output: 8
+ * Explanation: There are 8 valid tuples:
+ * (2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+ * (3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+ *
+ * Example 2:
+ * Input: nums = [1,2,4,5,10]
+ * Output: 16
+ * Explanation: There are 16 valid tuples:
+ * (1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+ * (2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+ * (2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+ * (4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+ *
+ * @see <a href="https://leetcode.cn/problems/tuple-with-same-product/" />
+ * @author Poet
+ * @date 2023/10/19
+ */
+public class LC_1726_TupleWithSameProduct {
+
+    /**
+     * 哈希统计
+     */
+    public int tupleSameProduct(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int key = nums[i] * nums[j];
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+        }
+
+        int res = 0;
+        for (Integer v : map.values()) {
+            res += v * (v - 1) * 4;
+        }
+        return res;
+    }
+
+}
