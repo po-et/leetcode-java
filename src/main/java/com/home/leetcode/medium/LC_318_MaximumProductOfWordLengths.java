e70425261d5e495fcad256939fa9df6141581d0e
Poet <ispoetmail@gmail.com>
2023-11-06 08:48:39 +0800

    Everyday(11.06): LC_318_MaximumProductOfWordLengths

---
+++ src/main/java/com/home/leetcode/medium/LC_318_MaximumProductOfWordLengths.java
@@ -0,0 +1,54 @@
+package com.home.leetcode.medium;
+
+/**
+ * 318. Maximum Product of Word Lengths
+ *
+ * Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. If no such two words exist, return 0.
+ *
+ * Example 1:
+ * Input: words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+ * Output: 16
+ * Explanation: The two words can be "abcw", "xtfn".
+ *
+ * Example 2:
+ * Input: words = ["a","ab","abc","d","cd","bcd","abcd"]
+ * Output: 4
+ * Explanation: The two words can be "ab", "cd".
+ *
+ * Example 3:
+ * Input: words = ["a","aa","aaa","aaaa"]
+ * Output: 0
+ * Explanation: No such pair of words.
+ *
+ * @see <a href="https://leetcode.cn/problems/maximum-product-of-word-lengths/" />
+ * @author Poet
+ * @date 2023/11/6
+ */
+public class LC_318_MaximumProductOfWordLengths {
+
+    /**
+     * 方法一：位运算
+     */
+    public int maxProduct(String[] words) {
+        int length = words.length;
+        int[] masks = new int[length];
+        for (int i = 0; i < length; i++) {
+            String word = words[i];
+            int wordLength = word.length();
+            for (int j = 0; j < wordLength; j++) {
+                masks[i] |= 1 << (word.charAt(j) - 'a');
+            }
+        }
+
+        int maxProd = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    maxProd = Math.max(maxProd, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return maxProd;
+    }
+
+}
